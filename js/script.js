4d09780c3d84cc6a2652e3e5e15e3af7beeb56c2
spavythra <spavythra@gmail.com>
2022-02-07 23:45:31 +0200

    first commit

---
+++ js/script.js
@@ -0,0 +1,7 @@
+/* ============================== typing animation ============================ */
+var typed = new Typed(".typing",{
+    strings:["","React developer","Python developer", "Aspiring full stack developer"],
+    typeSpeed:100,
+    BackSpeed:60,
+    loop:true
+})
