2d27e1fe3b26968b8b05f381fb04d424fb51c1e4
spavythra <spavythra@gmail.com>
2022-02-12 22:27:44 +0200

    Added transcript button, github links for projects and added an extra skillset.

--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
 /* ============================== typing animation ============================ */
 var typed = new Typed(".typing",{
-    strings:["","React developer","Python developer", "Aspiring full stack developer"],
+    strings:["","Front end developer","React developer","Python developer", "Aspiring full stack developer"],
     typeSpeed:100,
     BackSpeed:60,
     loop:true
