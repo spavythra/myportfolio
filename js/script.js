c95514a3eebe27f4ac794afc481df97e59dced73
spavythra <spavythra@gmail.com>
2022-06-01 22:32:11 +0300

    Updated the skills and its corresponding URLs

--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
 /* ============================== typing animation ============================ */
 var typed = new Typed(".typing",{
-    strings:["","Front end developer","React developer","Python developer", "Aspiring full stack developer"],
+    strings:["","Front-end developer","Back-end developer","Python developer", "Aspiring full-stack developer"],
     typeSpeed:100,
     BackSpeed:60,
     loop:true
