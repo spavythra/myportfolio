1479bf28e4f77b137c4855faa375751ab49405fd
spavythra <spavythra@gmail.com>
2022-06-07 22:55:38 +0300

    Updated UI colors

--- js/style-switcher.js
+++ js/style-switcher.js
@@ -28,17 +28,17 @@ function setActiveStyle(color)
 /* ========================== theme light and dark mode =========================== */
 const dayNight = document.querySelector(".day-night");
 dayNight.addEventListener("click", () => {
-    dayNight.querySelector("i").classList.toggle("fa-sun");
     dayNight.querySelector("i").classList.toggle("fa-moon");
+    dayNight.querySelector("i").classList.toggle("fa-sun");
     document.body.classList.toggle("dark");
 })
 window.addEventListener("load", () => {
     if(document.body.classList.contains("dark"))
     {
-        dayNight.querySelector("i").classList.add("fa-sun");
+        dayNight.querySelector("i").classList.add("fa-moon");
     }
     else
     {
-        dayNight.querySelector("i").classList.add("fa-moon");
+        dayNight.querySelector("i").classList.add("fa-sun");
     }
 })
