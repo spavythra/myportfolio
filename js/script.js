c955e6d256ed11ad326d873213dd5541ae4521ea
spavythra <spavythra@gmail.com>
2024-01-31 23:43:12 +0200

    (index.js, script.js): Updated job roles

--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
 /* ============================== typing animation ============================ */
 var typed = new Typed(".typing",{
-    strings:["","Front-end developer","Back-end developer", "Aspiring full-stack developer"],
+    strings:["","Web Developer","Cloud Engineer", "Aspiring full-stack developer"],
     typeSpeed:100,
     BackSpeed:60,
     loop:true
